b99f3b5e6c6abc65c663479bb9980e38fcaf400c
y0014984 <y0014984@gmail.com>
2023-11-17 18:48:52 +0100

    added basic map marker teleport

--- Scripts/Game/Map/Markers/GRAD_MapMarkerManagerComponent.c
+++ Scripts/Game/Map/Markers/GRAD_MapMarkerManagerComponent.c
@@ -22,8 +22,40 @@ modded class SCR_MapMarkerManagerComponent : SCR_BaseGameModeComponent
 			int markerPos[2];
 			marker.GetWorldPos(markerPos);
 			string markerText = marker.GetCustomText();
-			Print(string.Format("Custom Marker '%1' placed at pos %2", markerText, markerPos));
+			Print(string.Format("Custom Marker '%1' placed at pos %2", markerText, markerPos), LogLevel.NORMAL);
+			
+			markerText.ToLower();
+			if (markerText != "teleport")
+				return;
+			
+			array<int> playerIds = {};
+			GetGame().GetPlayerManager().GetPlayers(playerIds);
+			foreach (int playerId : playerIds)
+			{
+				BaseWorld world = GetGame().GetWorld();
+				IEntity playerEntity = GetGame().GetPlayerManager().GetPlayerControlledEntity(playerId);
+				
+				Print(string.Format("Player with ID %1 has position %2", playerId, playerEntity.GetOrigin()), LogLevel.NORMAL);
+				
+				vector worldPos = {markerPos[0], 500, markerPos[1]}; // start tracing in 500m height
+
+				vector outDir = {0, -1, 0}; // downward direction
+				outDir *= 1000; // trace for 1000 meters
+				
+		        autoptr TraceParam trace = new TraceParam();
+		        trace.Start = worldPos;
+		        trace.End = worldPos + outDir;
+		        trace.Flags = TraceFlags.WORLD | TraceFlags.OCEAN | TraceFlags.ENTS;
+		        trace.LayerMask = TRACE_LAYER_CAMERA;
+   
+		        float traceDis = world.TraceMove(trace, null);
+		        
+				vector newWorldPos = worldPos + outDir * traceDis;
+				
+				SCR_Global.TeleportPlayer(playerId, newWorldPos, SCR_EPlayerTeleportedReason.DEFAULT);
+				
+				Print(string.Format("Player with ID %1 teleported to position %2", playerId, newWorldPos), LogLevel.NORMAL);
+			}
 		}
 	}
-	
 }
